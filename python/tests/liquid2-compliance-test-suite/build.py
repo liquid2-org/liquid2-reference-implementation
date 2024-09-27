dec3c9bdbc8f6afe0e1083ac3f2cf8a6e097f420
James Prior <jamesgr.prior@gmail.com>
2024-09-27 21:30:42 +0100

    Test suite build script

---
+++ python/tests/liquid2-compliance-test-suite/build.py
@@ -0,0 +1,57 @@
+"""Liquid2 compliance test suite build script.
+
+This script and the accompanying JSON schema were heavily inspired by
+the JSONPath Compliance Test Suite.
+
+https://github.com/jsonpath-standard/jsonpath-compliance-test-suite
+"""
+
+import itertools
+import json
+import os
+import sys
+from pathlib import Path
+from typing import Any
+
+from jsonschema import validate
+
+ROOT = Path(__file__).parent / "tests"
+SCHEMA_PATH = Path(__file__).parent / "schema.json"
+
+with open(SCHEMA_PATH, encoding="utf-8") as fd:
+    SCHEMA = json.load(fd)
+
+
+def build() -> str:
+    files = ROOT.rglob("*.json")
+    tests = list(itertools.chain.from_iterable(load_tests(f) for f in files))
+
+    cts = {
+        "description": "Liquid2 compliance test suite",
+        "tests": tests,
+    }
+
+    return json.dumps(cts, indent=2)
+
+
+def load_tests(path: Path) -> list[dict[str, Any]]:
+    relative_path = path.relative_to(ROOT)
+    parts = relative_path.with_suffix("").parts
+    prefix = ", ".join(part.replace("_", " ") for part in parts)
+
+    sys.stderr.write(f"Loading {relative_path} with prefix '{prefix}'{os.linesep}")
+
+    with open(path, encoding="utf8") as fd:
+        tests = json.load(fd)
+
+    validate(instance=tests, schema=SCHEMA)
+    return [add_prefix(prefix, test) for test in tests["tests"]]
+
+
+def add_prefix(prefix: str, test: dict[str, Any]) -> dict[str, Any]:
+    name = prefix + ", " + test["name"]
+    return {**test, "name": name}
+
+
+if __name__ == "__main__":
+    sys.stdout.write(build())
--- python/tests/test_compliance.py
+++ python/tests/test_compliance.py
@@ -1,5 +1,6 @@
 """Test against the Liquid2 compliance test suite."""
 
+import asyncio
 import json
 import operator
 from dataclasses import dataclass
@@ -8,8 +9,7 @@ from typing import Any
 
 import pytest
 from liquid2 import Environment
-
-env = Environment()
+from liquid2.exceptions import Error
 
 
 @dataclass
@@ -24,28 +24,54 @@ class Case:
     tags: list[str] = field(default_factory=list)
 
 
-# TODO:
-FILENAME = "python/tests/liquid2-compliance-test-suite/tests/tags/assign.json"
+FILENAME = "python/tests/liquid2-compliance-test-suite/cts.json"
 
 
 def cases() -> list[Case]:
-    """Read test cases."""
     with open(FILENAME, encoding="utf8") as fd:
         data = json.load(fd)
     return [Case(**case) for case in data["tests"]]
 
 
 def valid_cases() -> list[Case]:
-    """Return all valid test cases."""
     return [case for case in cases() if not case.invalid]
 
 
 def invalid_cases() -> list[Case]:
-    """Return all invalid test cases."""
     return [case for case in cases() if case.invalid]
 
 
+@pytest.fixture
+def env() -> Environment:
+    return Environment()
+
+
 @pytest.mark.parametrize("case", valid_cases(), ids=operator.attrgetter("name"))
-def test_compliance(case: Case) -> None:
-    """Test valid templates."""
+def test_compliance(env: Environment, case: Case) -> None:
     assert env.from_string(case.template).render(**case.data) == case.result
+
+
+@pytest.mark.parametrize("case", valid_cases(), ids=operator.attrgetter("name"))
+def test_compliance_async(env: Environment, case: Case) -> None:
+    template = env.from_string(case.template)
+
+    async def coro() -> str:
+        return template.render(**case.data)
+
+    assert asyncio.run(coro()) == case.result
+
+
+@pytest.mark.parametrize("case", invalid_cases(), ids=operator.attrgetter("name"))
+def test_invalid_compliance(env: Environment, case: Case) -> None:
+    with pytest.raises(Error):
+        env.from_string(case.template).render(**case.data)
+
+
+@pytest.mark.parametrize("case", invalid_cases(), ids=operator.attrgetter("name"))
+def test_invalid_compliance_async(env: Environment, case: Case) -> None:
+    async def coro() -> str:
+        template = env.from_string(case.template)
+        return template.render(**case.data)
+
+    with pytest.raises(Error):
+        asyncio.run(coro())
